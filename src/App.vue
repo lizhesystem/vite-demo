9e70c4a70169e4f39eefc3e35abd5652ce71a27b
Administrator <lizhesystem@126.com>
2022-06-23 22:32:58 +0800

    TEST COMMIT

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <layout />
+	<layout/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 import layout from './views/layout/index.vue'
 
 export default defineComponent({
-  components: {
-    layout
-  },
-  name: 'App',
-  setup() {}
+	components: {
+		layout
+	},
+	name: 'App',
+	setup() {
+	}
 })
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background-color: #fff;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #fff;
 }
 </style>
